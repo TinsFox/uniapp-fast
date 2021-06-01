23cbca399785e0bf51e1b813167a9e846c45b808
TinsFox <fox@tinsfox.com>
2021-06-01 14:18:53 +0800

    feat(hooks): 集成Commitizen实现规范提交
    集成Commitizen实现规范提交，提交代码时使用`git cz`代替`git commit`

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: ['plugin:vue/essential', 'airbnb-base', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['vue'],
+  rules: {}
+};
